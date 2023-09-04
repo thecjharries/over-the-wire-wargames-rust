f4290a655036d23113e1873e61cd9aa2362f1ea3
CJ Harries <cj@wizardsoftheweb.pro>
2023-09-04 18:04:57 -0500

    Test bandit level9_password

--- src/bandit/mod.rs
+++ src/bandit/mod.rs
@@ -102,6 +102,15 @@ mod tests {
     #[allow(unused_imports)]
     use crate::get_ssh_client_from_settings_with_password;
 
+    #[tokio::test]
+    async fn level8_password_returns_proper_value() {
+        let client =
+            get_ssh_client_from_settings_with_password("bandit", 9, level9_password().await).await;
+        let result = client.execute("echo hello").await.unwrap();
+        assert_eq!("hello\n", result.stdout);
+        assert_eq!(0, result.exit_status);
+    }
+
     // #[tokio::test]
     // async fn level8_password_returns_proper_value() {
     //     let client =
